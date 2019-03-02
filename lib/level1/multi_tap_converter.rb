d4ddf8d2c84a40b8ddfe2fefba2ac39f6912c932
Tony KINO <tony.kino.3@gmail.com>
2019-03-02 14:07:50 +0100

    Refactoring naming in multi_tap_converter + rubocop

--- lib/level1/multi_tap_converter.rb
+++ lib/level1/multi_tap_converter.rb
@@ -4,51 +4,48 @@ require_relative 'keypad'
 
 class MultiTapConverter
   def multi_tap_to_word(input)
-    encoded_chars = encoded_string_to_encoded_chars(input)
-    decode_all_chars(encoded_chars).join
+    splitted_taps = input.scan(/2+|3+|4+|5+|6+|7+|8+|9+|0+/)
+    taps_to_chars(splitted_taps).join
   end
 
   def word_to_multi_tap(input)
-    chars = input.chars
-    encode_all_chars(chars).join
+    splitted_chars = input.chars
+    chars_to_taps(splitted_chars).join
   end
 
   private
 
-    def encoded_string_to_encoded_chars(str)
-      str.scan(/2+|3+|4+|5+|6+|7+|8+|9+|0+/)
+    def taps_to_chars(taps)
+      taps.map(&method(:tap_to_char))
     end
 
-    def decode_all_chars(encoded_chars)
-      encoded_chars.map(&method(:decode))
+    def tap_to_char(tap)
+      KeyPad::CORRESPONDENCE_TABLE.key(tap)
     end
 
-    def decode(encoded_char)
-      KeyPad::CORRESPONDENCE_TABLE.key(encoded_char)
-    end
-
-    def encode_all_chars(chars)
-      # I would like to use map the same way as decode_all_chars method forward
-      # But I can't see how to pass in argument the building array (encoded_chars below)
-      # (Pas certain du tout de mon anglais, je fais référence au array encoded_chars qui serait renvoyé par map que j'utilise au fur et à mesure de la boucle)
+    def chars_to_taps(chars)
+      # I would like to use map the same way as taps_to_chars method forward
+      # But I can't see how to pass in argument the building array taps
+      # (Pas certain du tout de mon anglais, je fais référence au array taps qui serait renvoyé par map que j'utilise au fur et à mesure de la boucle)
       # It seems it use some Proc knowledge that I am far to master
-      # chars.map(&method(:encode_and_add_space_if_needed ?))
-      encoded_chars = []
+      # chars.map(&method(:char_to_tap_and_add_space_if_needed ?))
+      taps = []
       chars.each do |char|
-        encoded_chars << encode_and_add_space_if_needed(encoded_chars, char)
+        taps << char_to_tap_and_add_space_if_needed(taps, char)
       end
-      encoded_chars
+      taps
     end
 
     # This method doesn't seem very 'clean' to me, does it exist a better way to write it ?
     # I tried to put the condition in another method "need_to_add_a_space?" who returned a boolean
     # but I am not convinced it's usefull
-    def encode_and_add_space_if_needed(encoded_chars, char)
-      return " #{encode(char)}" if encoded_chars.any? && encoded_chars.last[-1] == encode(char)[0]
-      encode(char)
+    def char_to_tap_and_add_space_if_needed(taps, char)
+      return " #{char_to_tap(char)}" if taps.any? && taps.last[-1] == char_to_tap(char)[0]
+
+      char_to_tap(char)
     end
 
-    def encode(char)
+    def char_to_tap(char)
       KeyPad::CORRESPONDENCE_TABLE.fetch(char)
     end
 end
--- lib/level1/multi_tap_to_word.rb
+++ lib/level1/multi_tap_to_word.rb
@@ -1,5 +1,7 @@
-# require_relative 'keypad'
-#
+# frozen_string_literal: true
+
+require_relative 'keypad'
+
 # def multi_tap_to_word(input)
 #   splitted_taps = input.scan(/2+|3+|4+|5+|6+|7+|8+|9+|0+/)
 #   taps_to_chars(splitted_taps).join
@@ -8,17 +10,8 @@
 # def taps_to_chars(taps)
 #   taps.map(&method(:tap_to_char))
 # end
-#
-# def tap_to_char(tap)
-#   KeyPad::CORRESPONDENCE_TABLE.key(tap)
-# end
-#
-# p multi_tap_to_word(ARGV[0])
-#
 
 # One liner version
-
-require_relative 'keypad'
 
 def multi_tap_to_word(input)
   input.scan(/2+|3+|4+|5+|6+|7+|8+|9+|0+/).map(&method(:tap_to_char)).join
