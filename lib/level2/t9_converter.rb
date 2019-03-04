f64a140f3b8478aa5ef9fc8470e9ec4740d4e196
Tony KINO <tony.kino.3@gmail.com>
2019-03-04 19:14:43 +0100

    Level2 works

---
+++ lib/level2/t9_converter.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative 'keypad'
+
+class T9Converter
+  def initialize
+    @words = get_word_list
+  end
+
+  def t9_to_word(seq)
+    possible_words(seq)[seq.count('1')]
+  end
+
+  def word_to_t9(seq)
+    t9_word = word_to_taps(seq)
+    add_ones_if_needed(t9_word, seq)
+  end
+
+  private
+
+    def get_word_list
+      Dir.chdir(File.dirname(__FILE__)) # permit to call ruby script from anywhere
+      File.readlines('word.txt', chomp: true)
+    end
+
+    def possible_words(seq)
+      @words.select { |word| seq.delete('1') == word_to_taps(word) }
+    end
+
+    def word_to_taps(seq)
+      seq.chars.map(&method(:char_to_tap)).join
+    end
+
+    def char_to_tap(char)
+      KeyPad::T9_CORRESPONDENCE_TABLE.find { |k, v| v.include?(char) }.first
+    end
+
+    def add_ones_if_needed(t9_word, seq)
+      word_position = possible_words(t9_word).index(seq)
+      word_position.times { t9_word << '1' }
+      t9_word
+    end
+
+end
+
+if $PROGRAM_NAME == __FILE__
+  # p T9Converter.new.t9_to_word('843111')
+  p T9Converter.new.word_to_t9('vie')
+end
